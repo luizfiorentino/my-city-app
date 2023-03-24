b92b8c5ddf84e9321604f13829ff6beca3ca3793
luizfiorentino <lgfiorentino@gmail.com>
2023-03-24 16:33:48 +0100

    feat- an issue can be posted via an axios request

--- components/FormContent/FormContent.jsx
+++ components/FormContent/FormContent.jsx
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./FormContent.module.css";
 import FormHeader from "../FormHeader/FormHeader";
 import FormSubtitle from "../FormSubtitle/FormSubtitle";
 import FormInput from "../FormInput/FormInput";
 import Footer from "../Footer/Footer";
+
+import axios from "axios";
+
 export default function FormContent() {
+  const [userName, setUserName] = useState("");
+  const [description, setDescription] = useState("");
+  const [location, setLocation] = useState("");
+
+  //console.log("form", userName, description, location);
+
+  // const setInfos = (e) => {
+  //   setUserName
+  // }
+
+  //   const handleSubmit = (event) => {
+  //     // prevents the submit button from refreshing the page
+  //     event.preventDefault();
+  //     console.log(contactInfo);
+  //   };
+
+  // <form onSubmit={handleSubmit}>
+
+  const issueRequest = async (req, res) => {
+    try {
+      const newIssue = await axios.post(`http://localhost:3000/api/issues`, {
+        issue: {
+          userName: userName,
+          description: description,
+          location: location,
+        },
+      });
+
+      // {"issue":{"userName":"Jonah Pineda", "description":"There is a lot of trash in the street", "location": "Beracostraat 40, Amsterdam"}}
+
+      return res
+        .status(201)
+        .json({ message: `Yep new issue in!`, newIssue: newIssue });
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
   return (
     <div className={styles.main}>
       <div className={styles.mainInner}>
@@ -13,17 +54,28 @@ export default function FormContent() {
           <FormSubtitle />
         </div>
         <div className={styles.inputFields}>
-          <FormInput label="Name" placeHolder="e.g. Mike Ness" />
+          <FormInput
+            label="Name"
+            placeHolder="e.g. Mike Ness"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
           <FormInput
             label="Description"
             placeHolder="e.g. there is something..."
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
           />
           <FormInput
             label="Location"
             placeHolder="e.g. Dijkstraat 123. Amsterdam"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
           />
         </div>
       </div>
+
+      <button onClick={issueRequest}>click</button>
 
       <Footer />
     </div>
--- components/FormInput/FormInput.jsx
+++ components/FormInput/FormInput.jsx
@@ -2,10 +2,18 @@ import React from "react";
 import styles from "./FormInput.module.css";
 
 export default function FormInput(props) {
+  // const { userName, description, location, name, issue, place } =
+  //   issuesContext();
+  //console.log("props", props);
   return (
     <div className={styles.main}>
       <label>{props.label}</label>
-      <input className={styles.placeholder} placeholder={props.placeHolder} />
+      <input
+        className={styles.placeholder}
+        placeholder={props.placeHolder}
+        value={props.value}
+        onChange={(e) => props.onChange(e)}
+      />
     </div>
   );
 }
