389ddda18dbf0da71c3a1e7b99fea26132150f5d
luizfiorentino <lgfiorentino@gmail.com>
2023-03-29 21:21:19 +0200

    AdminIssuesBar styled with FilterSelector and NewIssue icon

--- components/adminTopBar/AdminTopBar.jsx
+++ components/adminTopBar/AdminTopBar.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import styles from "./AdminTopBar.module.css";
 import AdminTbIcon from "../adminTopBarIcon/AdminTbIcon";
-import imgAvatar from "../../pages/assets/images/image-avatar.jpg";
 import Avatar from "../avatar/Avatar";
 
 export default function AdminTopBar() {
   return (
     <div className={styles.main}>
       <AdminTbIcon />
-
       <Avatar />
     </div>
   );
