17a96d75eb105b8881b12c82a54ac3758efe400f
luizfiorentino <lgfiorentino@gmail.com>
2023-04-05 13:32:40 +0200

    [issueId].jsx subpage fetching the right issue with getServerSideProps

--- pages/admin/issues/[issueId].jsx
+++ pages/admin/issues/[issueId].jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { useRouter } from "next/router";
-//import prisma from "@/prisma/client";
+import prisma from "@/prisma/client";
+import serialize from "@/utils/serialize";
 
-export default function IssueStatus({ issues }) {
-  //   const router = useRouter();
-  //   const id = router.asPath;
-  //console.log("issue det page", issues);
+export default function IssueStatus({ issue }) {
+  // const router = useRouter();
+  // const id = router.asPath.substring(14);
+  console.log("issue det page top", issue);
 
   return (
     <div>
@@ -14,27 +14,16 @@ export default function IssueStatus({ issues }) {
   );
 }
 
-export async function getServerSideProps() {
+export async function getServerSideProps(context) {
   try {
-    const router = useRouter();
-    const id = router.asPath;
-    console.log("id-->", id);
     const issue = await prisma.issue.findUnique({
       where: {
-        id: id,
+        id: context.params.issueId,
       },
     });
+
     return { props: { issue: serialize(issue) } };
   } catch (e) {
     console.log("db error:", e);
   }
 }
-
-// export async function getServerSideProps() {
-//   try {
-//     const issues = await prisma.issue.findMany();
-//     return { props: { issues: serialize(issues) } };
-//   } catch (e) {
-//     console.log("db error:", e);
-//   }
-// }
