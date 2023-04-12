1fdb82ed6b499a4829935e5c8f6e301718ab42ca
luizfiorentino <lgfiorentino@gmail.com>
2023-04-12 20:04:55 +0200

    Admin TobBarIcon styled

--- components/Admin/Nav/AdminTopBarIcon/AdminTbIcon.jsx
+++ components/Admin/Nav/AdminTopBarIcon/AdminTbIcon.jsx
@@ -2,5 +2,16 @@ import React from "react";
 import styles from "./AdminTbIcon.module.css";
 
 export default function AdminTbIcon() {
-  return <p className={styles.main}></p>;
+  return (
+    <p className={styles.main}>
+      <div className={styles.pacmanUp}>
+        <div className={styles.pacmanInner}>
+          {/* <span className={styles.triangle}></span> */}
+        </div>
+      </div>
+      <div className={styles.pacman}></div>
+
+      <div className={styles.screen}></div>
+    </p>
+  );
 }
