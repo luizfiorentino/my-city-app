6cadc41eaa43dcfacf3f1c5903dc4ff0d15dafe4
luizfiorentino <lgfiorentino@gmail.com>
2023-04-13 15:11:05 +0200

    Link to the UserForm added in the "+ New" button of the Admin List

--- components/Admin/List/ReportsBar/ReportsBar.jsx
+++ components/Admin/List/ReportsBar/ReportsBar.jsx
@@ -4,6 +4,7 @@ import arrowIcon from "../../../../pages/assets/images/icon-arrow-down.svg";
 import TextParagraph from "../../Shared/Typography/TextParagraph";
 import TextBold from "../../Shared/Typography/TextBold";
 import { leagueSpartan } from "@/styles/fonts";
+import Link from "next/link";
 
 export default function ReportsBar(props) {
   return (
@@ -24,13 +25,15 @@ export default function ReportsBar(props) {
             src={arrowIcon.src}
             alt="arrow down icon"
           />
-        </TextBold>
-        <p className={`${styles.newIssue} ${leagueSpartan.className}`}>
-          <span className={styles.addIcon}>
-            <span className={styles.plusSign}>+</span>
-          </span>{" "}
-          New
-        </p>
+        </TextBold>{" "}
+        <Link href="/">
+          <p className={`${styles.newIssue} ${leagueSpartan.className}`}>
+            <span className={styles.addIcon}>
+              <span className={styles.plusSign}>+</span>
+            </span>{" "}
+            New
+          </p>{" "}
+        </Link>
       </div>
     </div>
   );
