fc5c4be87de05aea36a6861d4ce4fe49d1448328
luizfiorentino <lgfiorentino@gmail.com>
2023-07-03 15:14:26 +0200

    UserLocation component using react-leaflet DraggableMarker for the location map.

--- components/User/IssueForm/UserLoaction/UserLocation.jsx
+++ components/User/IssueForm/UserLoaction/UserLocation.jsx
@@ -1,5 +1,5 @@
-import { useContext } from "react";
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { useContext, useState, useRef, useMemo, useCallback } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "maplibre-gl/dist/maplibre-gl.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
@@ -16,21 +16,61 @@ function UserLocation() {
   const longitude = context.longitude ? context.longitude : "4.8914";
 
   const markerPosition = [latitude, longitude];
+  const center = [latitude, longitude];
+
+  function DraggableMarker() {
+    const [draggable, setDraggable] = useState(false);
+    const [position, setPosition] = useState(center);
+    const markerRef = useRef(null);
+    const eventHandlers = useMemo(
+      () => ({
+        dragend() {
+          const marker = markerRef.current;
+          if (marker != null) {
+            setPosition(marker.getLatLng());
+          }
+        },
+      }),
+      []
+    );
+    const toggleDraggable = useCallback(() => {
+      setDraggable((d) => !d);
+    }, []);
+
+    return (
+      <Marker
+        draggable={draggable}
+        eventHandlers={eventHandlers}
+        position={position}
+        ref={markerRef}
+      >
+        <Popup minWidth={90}>
+          <span onClick={toggleDraggable}>
+            {draggable
+              ? "Marker is draggable"
+              : "Click here to make marker draggable"}
+          </span>
+        </Popup>
+      </Marker>
+    );
+  }
 
   return (
     <div className={styles.mainContainer}>
       {context.latitude ? (
         <MapContainer
           // center={[latitude, longitude]}
-          center={markerPosition}
+          center={center}
           zoom={14}
           style={{ width: "100%", height: "180px" }}
+          scrollWheelZoom={false}
         >
           <TileLayer
             attribution={`&copy; MapTiler &copy; OpenStreetMap contributors`}
             url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=Zlvdjhx8LLJZkQKgusKO"
           />
-          <Marker position={markerPosition} />
+          {/* <Marker position={markerPosition} /> */}
+          <DraggableMarker />
         </MapContainer>
       ) : (
         <p>
