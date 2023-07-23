9d2cd8f3faa497bbf6c6b0c6d205067a4cec0a37
luizfiorentino <lgfiorentino@gmail.com>
2023-07-23 16:19:39 +0200

    Conflict resolution.

--- components/User/IssueForm/StepTwoForm/StepTwoForm.jsx
+++ components/User/IssueForm/StepTwoForm/StepTwoForm.jsx
@@ -16,6 +16,7 @@ export default function StepTwoForm() {
     longitude,
     setLongitude,
     setSelectedStepForm,
+    setStepOneFormData,
   } = useContext(IssueContext);
 
   useEffect(() => {
