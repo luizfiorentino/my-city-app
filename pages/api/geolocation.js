14f2b5a21638f1bedaab0f5cc0a96e978424b342
luizfiorentino <lgfiorentino@gmail.com>
2023-07-23 15:57:45 +0200

    Console.logs added in the geolocationApiCall and related function for debugging purposes.

--- pages/api/geolocation.js
+++ pages/api/geolocation.js
@@ -1,12 +1,19 @@
 import fetch from "node-fetch";
 
 const apiKey = process.env.OPENCAGE_API_KEY;
+console.log("API key?", apiKey);
 const rateLimitWindowMs = 60000;
 const maxRequestsPerWindow = 100;
 const rateLimits = new Map();
 const allowedDomains = [
+<<<<<<< HEAD
   "http://localhost:3000",
   "https://my-city-app.vercel.app",
+=======
+  "https://my-city-app-git-main-luizfiorentino.vercel.app",
+  ,
+  "http://localhost:3000",
+>>>>>>> b85256f (Console.logs added in the geolocationApiCall and related function for debugging purposes.)
 ];
 
 export default async function handler(req, res) {
@@ -51,14 +58,16 @@ export default async function handler(req, res) {
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
+    console.log("/api/geolocation handler, response", response);
 
     if (data.results.length > 0) {
       const address = data.results[0].formatted;
       res.json({ address });
     } else {
-      res
-        .status(404)
-        .json({ error: "No address found for the given coordinates." });
+      res.status(404).json({
+        error:
+          "(api/geolocation handler)No address found for the given coordinates.",
+      });
     }
   } catch (error) {
     res.status(500).json({ error: "An error occurred." });
