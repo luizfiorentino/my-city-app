5c1eab96eb08603bc55440a3a1b8959d546b963d
luizfiorentino <lgfiorentino@gmail.com>
2023-08-01 06:59:25 +0200

    Bug fix- a) submit button with correct loading state and b) map with the issue location being correctly displayed in the Details Page.

--- components/User/IssueForm/UserForm/UserForm.jsx
+++ components/User/IssueForm/UserForm/UserForm.jsx
@@ -18,7 +18,6 @@ const formSteps = ["INFOS", "LOCATION", "PICTURES", "CONFIRM DATA"];
 export default function UserForm() {
   const context = useContext(IssueContext);
   const [loading, setLoading] = useState(false);
-  console.log("userform context", context);
 
   const issueRequest = async () => {
     const { stepOneFormData, location } = context;
@@ -97,7 +96,7 @@ export default function UserForm() {
         {context.selectedStepForm === "LOCATION" && <StepTwoForm />}
         {context.selectedStepForm === "PICTURES" && <StepThreeForm />}
         {context.selectedStepForm === "CONFIRM DATA" && (
-          <StepFourForm issueRequest={issueRequest} loading={loading} />
+          <StepFourForm issueRequest={issueRequest} />
         )}
 
         {context.selectedStepForm === "SUBMITTED" && (
