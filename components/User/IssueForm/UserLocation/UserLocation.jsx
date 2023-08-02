32796678be71c5dc75d8e98717ee0a1acb662d41
luizfiorentino <lgfiorentino@gmail.com>
2023-08-02 09:14:17 +0200

    Latest version. Css improvements.

--- components/User/IssueForm/UserLocation/UserLocation.jsx
+++ components/User/IssueForm/UserLocation/UserLocation.jsx
@@ -78,6 +78,7 @@ function UserLocation(props) {
           <TileLayer
             attribution={`&copy; MapTiler &copy; OpenStreetMap contributors`}
             url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=Zlvdjhx8LLJZkQKgusKO"
+
             //Alternatively (also works!)
             // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             // attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
